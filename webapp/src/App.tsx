f7b86ed2c7d43cdea14bf5a15fbaccd3de89a09b
kostyuchenko <kostyu444enko.igor@gmail.com>
2025-11-21 17:39:56 +0300

    feat: добавить tRPC во фронтенд

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,16 +1,10 @@
-export const App = () => {
+import { TrpcProvider } from "./lib/trpc";
+import { AllIdeasPage } from "./pages/AllIdeasPage";
 
+export const App = () => {
     return (
-        <div>
-            <h1>Ideas</h1>
-            {ideas.map((el, key) => {
-                return (
-                    <div key={key}>
-                        <h2>{el.name}</h2>
-                        <p>{el.description}</p>
-                    </div>
-                );
-            })}
-        </div>
-    );
+        <TrpcProvider>
+            <AllIdeasPage />
+        </TrpcProvider>
+    )
 };
