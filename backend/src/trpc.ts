58a2b2317dbf887669f917cfb969bd8893b1fa84
kostyuchenko <kostyu444enko.igor@gmail.com>
2025-12-28 17:41:02 +0300

    staged test 2

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -11,7 +11,9 @@ const ideas = [
 const trpc = initTRPC.create();
 
 const x: string = "hello";
-if (Math.random()) console.info(x);
+if (Math.random() + 1) {
+    console.info(x);
+}
 export const trpcRouter = trpc.router({
     getIdeas: trpc.procedure.query(() => {
         return { ideas };
