2d97197013858b32ba148104406f7bc224217878
kostyuchenko <kostyu444enko.igor@gmail.com>
2025-12-28 22:40:46 +0300

    Feat: add react-router-dom

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { TrpcProvider } from "./lib/trpc";
 import { AllIdeasPage } from "./pages/AllIdeasPage";
+import { ViewIdeaPage } from "./pages/ViewIdeaPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { getAllIdeasRoute, getViewIdeaRoute } from "./lib/routes";
+
 export const App = () => {
     return (
         <TrpcProvider>
-            <AllIdeasPage />
+            <BrowserRouter>
+                <Routes>
+                    <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+                    <Route path={getViewIdeaRoute({ ideaNick: ":ideaNick" })} element={<ViewIdeaPage />} />
+                </Routes>
+            </BrowserRouter>
         </TrpcProvider>
     );
 };
--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { trpc } from "../../lib/trpc";
+import { Link } from "react-router-dom";
+import { getViewIdeaRoute } from "../../lib/routes";
 
 export const AllIdeasPage = () => {
     const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery();
@@ -12,11 +14,13 @@ export const AllIdeasPage = () => {
     return (
         <div>
             <h1>All Ideas</h1>
-            {data.ideas.map((el, key) => {
+            {data.ideas.map((idea, key) => {
                 return (
                     <div key={key}>
-                        <h2>{el.name}</h2>
-                        <p>{el.description}</p>
+                        <h2>
+                            <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>{idea.name}</Link>
+                        </h2>
+                        <p>{idea.description}</p>
                     </div>
                 );
             })}
