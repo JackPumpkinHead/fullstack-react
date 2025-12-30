56befb3eb0486aa7a72d459466e250932f38b9e4
kostyuchenko <kostyu444enko.igor@gmail.com>
2025-12-30 12:48:35 +0300

    Feat: add Layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,14 +4,17 @@ import { AllIdeasPage } from "./pages/AllIdeasPage";
 import { ViewIdeaPage } from "./pages/ViewIdeaPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from "./lib/routes";
+import { Layout } from "./components/Layout";
 
 export const App = () => {
     return (
         <TrpcProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-                    <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+                    <Route element={<Layout />}>
+                        <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+                        <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </TrpcProvider>
---
+++ webapp/src/components/Layout/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { getAllIdeasRoute } from "../../lib/routes";
+
+export const Layout = () => {
+    return (
+        <div>
+            <p>
+                <b>IdeaNick</b>
+            </p>
+            <ul>
+                <li>
+                    <Link to={getAllIdeasRoute()}>All Ideas</Link>
+                </li>
+            </ul>
+            <hr />
+            <div>
+                <Outlet />
+            </div>
+        </div>
+    );
+};
